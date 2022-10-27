e2f2c4a40274b4875e1984dafd74d0f95b59f37e
benjamindalyndiaye <benjamindalyndiaye@esp.sn>
2022-10-27 23:36:54 +0000

    first commit

---
+++ apiAuthentification-master/app/Http/Controllers/Api/PassportAuthController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PassportAuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        echo($request);
+        $this->validate($request, [
+            'name' => 'required|min:4',
+            'email' => 'required|email',
+            'password' => 'required|min:8',
+        ]);
+  
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);  
+
+        return response()->json($user, 200);
+        
+    }
+  
+    /**
+     * Login Req
+     */
+    public function login(Request $request)
+    {
+        $data = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+  
+        if (auth()->attempt($data)) {
+            return response()->json(['user' => $data], 200);
+        } else {
+            return response()->json(['error' => 'Unauthorised'], 401);
+        }
+    }
+}
